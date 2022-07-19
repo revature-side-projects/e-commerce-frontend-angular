63d2b97e083f90176da2ae5ef3f416c7e3bba1ac
manthakali <manthakali@gmail.com>
2022-07-19 14:21:38 -0400

    input validation form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
   registerForm = new FormGroup({
-    fname: new FormControl(''),
-    lname: new FormControl(''),
+    fname: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    lname: new FormControl('', [Validators.required, Validators.maxLength(10)]),
     email: new FormControl(''),
-    password: new FormControl('')
-  })
-  
+    password: new FormControl('', [Validators.required, Validators.minLength(10)])
+  });
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -24,6 +25,7 @@ export class RegisterComponent implements OnInit {
   }
   
   onSubmit(): void {
+   
     this.authService.register(this.registerForm.get('fname')?.value, this.registerForm.get('lname')?.value, this.registerForm.get('email')?.value, this.registerForm.get('password')?.value).subscribe(
       () => console.log("New user registered"),
       (err) => console.log(err),
@@ -32,3 +34,9 @@ export class RegisterComponent implements OnInit {
   }
 
 }
+  
+  
+
+  
+
+
