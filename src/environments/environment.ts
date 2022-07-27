753ad8cae5aa0f5e2373a67d27357ee8f766d92f
Saber <Stephen.sams@live.com>
2022-07-27 07:34:35 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,15 @@ export const environment = {
   },
 };
 
-
+export const environment = {
+  production: true,
+  withCredentials: true,
+  baseUrl: "http://backend-env.eba-g9uchpeu.us-west-2.elasticbeanstalk.com",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -22,3 +30,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
