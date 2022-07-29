4a0613f0eb0b385deb88fa5a28292a60ff8beeed
Kenneth Burke <khtenne@gmail.com>
2022-07-28 17:33:42 -0700

    AppComponent test complete 100

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from 'src/app/app.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core/';
 
 describe('AppComponent', () => {
+
+  
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [{
+	        provide: AppComponent, useValue: {}
+      }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
@@ -14,18 +22,25 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project3Demo'`, () => {
+  it(`should have as title 'E-Commerce Client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('project3Demo');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('E-Commerce Client');
   });
 
-  it('should render title', () => {
+  it('should have as searchProducts [] ', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'project3Demo app is running!'
-    );
+    const app = fixture.debugElement.componentInstance;
+    expect (app.searchProducts).toEqual([]);
+    
   });
-});
+  
+  it(`should have as search 'help'`, ()=>{
+	const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect (app.search).toEqual('help');
+  
+	
+  });
+  
+ })
