ca44766753d1eab044f452199716d9cbfd67fb9e
Kenneth Burke <khtenne@gmail.com>
2022-07-30 13:55:31 -0700

    added review.ts model, changed review service methods to be observables

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,42 +1,41 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+
+import {Review} from 'src/app/models/review';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReviewService {
   reviewsUrl: string = `${environment.baseUrl}/api/review`;
-
+  httpOptions = { 
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+}
   constructor(private http: HttpClient) {}
 
   // Get a list of all reviews for all products
-  getAllReviews() {
-    return this.http.get(this.reviewsUrl, { headers: environment.headers });
+  getAllReviews(): Observable<Review[]>{
+    return this.http.get<Review[]>(this.reviewsUrl, this.httpOptions);
   }
 
   // Get a list of reviews related to a product with the given product ID
-  getProductReviews(productId: number) {
-    return this.http.get(`${this.reviewsUrl}/product/${productId}`, {
-      headers: environment.headers,
-    });
+  getProductReviews(productId: number): Observable<Review> {
+    return this.http.get<Review>(`${this.reviewsUrl}/product/${productId}`, this.httpOptions);
   }
 
   // Get a list of reviews written by the user with the given user ID
-  getUsersReviews(userId: number) {
-    return this.http.get(`${this.reviewsUrl}/user/${userId}`, {
-      headers: environment.headers,
-    });
+  getUsersReviews(userId: number): Observable<Review[]> {
+    return this.http.get<Review[]>(`${this.reviewsUrl}/user/${userId}`, this.httpOptions);
   }
 
   // Get a specific review by its ID
-  getReviewById(id: number) {
-    return this.http.get(`${this.reviewsUrl}/${id}`, {
-      headers: environment.headers,
-    });
+  getReviewById(id: number): Observable<Review> {
+    return this.http.get<Review>(`${this.reviewsUrl}/${id}`, this.httpOptions);
   }
 
-  postReview(productId: number, stars: number, title: string, review: string) {
+  postReview(productId: number, stars: number, title: string, review: string): Observable<Review>  {
     if (stars > 5) {
       stars = 5;
     }
@@ -47,14 +46,14 @@ export class ReviewService {
       title: title,
       review: review,
     };
-    return this.http.post(`${this.reviewsUrl}`, userReview, {
+    return this.http.post<Review>(`${this.reviewsUrl}`, userReview, {
       headers: environment.headers,
       withCredentials: environment.withCredentials,
     });
   }
 
-  deleteReviewById(id: number) {
-    return this.http.delete(`${this.reviewsUrl}/${id}`, {
+  deleteReviewById(id: number): Observable<Review> {
+    return this.http.delete<Review>(`${this.reviewsUrl}/${id}`, {
       headers: environment.headers,
       withCredentials: environment.withCredentials,
     });
