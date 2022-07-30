9946e8ef90771b904ab2c9438e915295b993fb2b
Sam <hengliew4@gmail.com>
2022-07-30 16:24:15 -0400

    attempt

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthenticationService} from "../services/authentication.service";
+import {User} from "../models/user";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authentication: AuthenticationService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let userString:any = localStorage.getItem('user');
+    let userJSON:User = JSON.parse(userString);
+
+    if (userJSON !== null && this.authentication.token !== undefined) {
+      request = request.clone({
+        headers: request.headers.set('authorization', 'Bearer ' + this.authentication.token)
+      })
+    }
+    return next.handle(request);
+  }
+}
+
+export const AuthInterceptorProvider = {
+  provide:HTTP_INTERCEPTORS,
+  useClass:AuthInterceptor,
+  multi: true,
+}
