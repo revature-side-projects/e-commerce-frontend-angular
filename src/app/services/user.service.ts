c3b54e420509fb049a04a5a6c1bcf86c90be4b93
Hector Orozco <hect05661@gmail.com>
2022-07-30 15:27:37 -0700

    Took care of a few code smells

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,4 @@
 import { environment } from './../../environments/environment';
-import { Address } from './../models/address';
 import { Injectable } from '@angular/core';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
