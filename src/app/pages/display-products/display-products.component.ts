1ec752266328cb7e569f718f159e00c8e9493cc3
Jheflin1995 <106621400+Jheflin1995@users.noreply.github.com>
2022-07-31 11:36:06 -0500

    changed delete product to modal

--- src/app/pages/display-products/display-products.component.ts
+++ src/app/pages/display-products/display-products.component.ts
@@ -6,6 +6,8 @@ import { AuthenticationService } from '../../services/authentication.service';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
 import { AppComponent } from '../../app.component';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-display-products',
@@ -16,13 +18,18 @@ export class DisplayProductsComponent implements OnInit {
   allProducts: Product[] = [];
   searchProducts: Product[] = [];
   role: string = 'GUEST';
+  updateModalVisibility: string = '';
+  deleteModalVisibility: string = '';
+  productToUpdate: Product = new Product(0,'',0,'',0,'');
+  productToDelete: Product = new Product(0,'',0,'',0,'');
 
   constructor(
     private productService: ProductService,
     public auth: AuthService,
     private userService: UserService,
     private authentication: AuthenticationService,
-    public appComponent: AppComponent
+    public appComponent: AppComponent,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -121,4 +128,80 @@ export class DisplayProductsComponent implements OnInit {
       }
     );
   }
+
+  updateProductForm = new FormGroup({
+    pname: new FormControl(''),
+    pquantity: new FormControl(''),
+    pdescription: new FormControl(''),
+    pprice: new FormControl(''),
+    pimage: new FormControl(''),
+  });
+
+  onSubmitUpdate(product: Product) {
+    let name: string;
+    let quantity: number;
+    let description: string;
+    let price: number;
+    let image: string;
+
+    if (this.updateProductForm.get('pname')?.value === '') {
+      name = product.name;
+    } else {
+      name = this.updateProductForm.get('pname')?.value;
+    }
+
+    if (this.updateProductForm.get('pquantity')?.value === '') {
+      quantity = product.quantity;
+    } else {
+      quantity = this.updateProductForm.get('pquantity')?.value;
+    }
+
+    if (this.updateProductForm.get('pdescription')?.value === '') {
+      description = product.description;
+    } else {
+      description = this.updateProductForm.get('pdescription')?.value;
+    }
+
+    if (this.updateProductForm.get('pprice')?.value === '') {
+      price = product.price;
+    } else {
+      price = this.updateProductForm.get('pprice')?.value;
+    }
+
+    if (this.updateProductForm.get('pimage')?.value === '') {
+      image = product.image;
+    } else {
+      image = 'https://revazon-image-bucket.s3.amazonaws.com/' + this.updateProductForm.get('pimage')?.value;
+    }
+    this.productService
+      .updateProduct(product.id, name, quantity, description, price, image)
+      .subscribe(
+        () => {
+          this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+          this.router.onSameUrlNavigation = 'reload';
+        },
+        (err) => console.log(err),
+        () => this.router.navigate([''])
+      );
+  }
+
+  onDeleteProduct(product: Product) {
+    this.productService.deleteProduct(product.id).subscribe(
+      () => {
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+      },
+      (err: any) => console.log(err),
+      () => this.router.navigate([''])
+    );
+  }
+
+  closePopup() {
+    this.updateModalVisibility = 'none';
+    this.deleteModalVisibility = 'none';
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+  }
+
+
 }
